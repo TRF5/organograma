20cd512c0971ddfdf476370fde22d2d04ae110cb
Roberta Vitória <66263681+roberta976@users.noreply.github.com>
2022-11-07 17:01:52 -0300

    Add files via upload

---
+++ template-parts/searchform.php
@@ -0,0 +1,9 @@
+<form id="pesquisa-header" class="row">
+  <div class="col">
+    <label for="pesquisa" class="visually-hidden">pesquisa</label>
+    <input type="text" class="form-control" id="pesquisa" placeholder="Escreva o que você deseja pesquisar no Sistema Organograma">
+  </div>
+  <div class="col">
+    <button type="submit" class="btn btn-primary mb-3">Pesquisar</button>
+  </div>
+</form>
