cf74664f06a143fd07b8bb3940858c512bfbf1a8
Roberta Vitória <66263681+roberta976@users.noreply.github.com>
2022-12-02 12:10:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-<?php
-require_once("./conf.php");
 
-$login = isset($_POST['login']) ? $_POST['login']: null; 
-$erros = array();
 
-if ($login) {
-	
-	require_once('./admin/sessao.class.php');
-	
-	$senha = isset($_POST['senha'])? $_POST['senha']: null;
-	
-	if (Sessao::autentica($login, $senha, $mensagemErro)) {
-		
-		header('Location: /organograma-atualizado/bem-vindo.php');
-		exit;
-	} else {
-		$erros[] = $mensagemErro;
-	}
-}
+<?php require_once('login.html.php'); ?>
 
-require_once('login.html.php');
-?>
 
