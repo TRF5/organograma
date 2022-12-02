3742359aff9eae8e1b5cade6df98e916219fed5d
Roberta Vitória <66263681+roberta976@users.noreply.github.com>
2022-12-02 12:10:40 -0300

    Update login.html.php

--- login.html.php
+++ login.html.php
@@ -48,12 +48,35 @@
                           <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                         </symbol>
                     </svg>
-                    <div id='alerta-senha' class="alert alert-danger align-items-center" role="alert">
-                        <svg class="bi flex-shrink-0 me-3" width="20" height="20" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                        <div>
-                            Login ou senha incorretos! Por favor, tente novamente.
-                        </div>
-                    </div>
+                    <?php
+                        require_once("./conf.php");
+
+                        $login = isset($_POST['login']) ? $_POST['login']: null; 
+                        $erros = array();
+
+                        if ($login) {
+                            
+                            require_once('./admin/sessao.class.php');
+                            
+                            $senha = isset($_POST['senha'])? $_POST['senha']: null;
+                            
+                            if (Sessao::autentica($login, $senha, $mensagemErro)) {
+                                
+                                header('Location: /organograma-atualizado/bem-vindo.php');
+                                exit;
+                            } else {
+                                
+                                echo '<div id="alerta-senha" class="alert alert-danger align-items-center" role="alert">
+                                        <svg class="bi flex-shrink-0 me-3" width="20" height="20" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                                        <div>
+                                            Login ou senha incorretos! Por favor, tente novamente.
+                                        </div>
+                                    </div>';
+                                
+                                $erros[] = $mensagemErro;
+                            }
+                        }
+                    ?>
                 </div>
 
 
